1efc01ab20a2024a869f0a1975ec4212d37ed9f3
kasiyah <assiyakalykova@gmail.com>
2025-05-08 23:34:08 -0500

    adding vm and linux notes

---
+++ Linux/linux.py
@@ -0,0 +1,32 @@
+"""
+Linux priciples
+1. Everything is a file (including Hardware)
+2. Small Single purpose programs
+3. Ability to chain programs together for complex operations
+4. Avoid Captive User Interface
+5. Configuration ata stored in text file
+
+Why Linux?
+1. Opensource
+2. Community Support
+3. Support Wide Variety of Hardwae
+4. Customization
+5. Most servers runs on Linux
+6. Automation
+7. Security
+
+Popular Linux Distros
+1. Ubuntu Linux
+2. Linux Mint
+3. Arch inux
+4. Fedora
+5. Debian
+6. OpenSuse
+
+Popuar Server Linux OS
+1. Red Hat Enterprise Linux
+2. Ubuntu Server 
+3. Centos
+4. SUSE Enterprise Linux
+"""
+
